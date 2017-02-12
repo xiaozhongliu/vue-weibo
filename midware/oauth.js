1f51a0c3fc510b64587e4363d1d9a58aa7ae74fb
XiaozhongLiu <xiaozhong.liu@outlook.com>
2017-02-12 21:39:48 +0800

    prod config further

--- midware/oauth.js
+++ midware/oauth.js
@@ -9,7 +9,7 @@ weibo.init('weibo', config.APP_KEY, config.APP_SECRET);
 
 module.exports = weibo.oauth({
     blogtypeField: 'type',
-    homeUrl: `http://${this.APP_DOMAIN}:${this.APP_PORT}`,
+    homeUrl: `http://${config.APP_DOMAIN}:${config.APP_PORT}`,
     afterLogin: function (req, res, callback) {
         console.log(req.session.oauthUser.screen_name, 'login success');
         process.nextTick(callback);
