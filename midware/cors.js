b7d0382f4bac4b799f6f8237cb937ef65256f5eb
XiaozhongLiu <xiaozhong.liu@outlook.com>
2017-02-13 14:48:59 +0800

    optimization for domain name which has no www prefix

--- midware/cors.js
+++ midware/cors.js
@@ -1,7 +1,14 @@
 const config = require('../config')();
+const IP_REG = /((?:(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))\.){3}(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d))))/;
 
 module.exports = (req, res, next) => {
 
+    // if is domain name and has no www.
+    if (!IP_REG.text(config.APP_DOMAIN) && !config.APP_DOMAIN.includes('www')) {
+        config.APP_DOMAIN = `www.${config.APP_DOMAIN}`
+    }
+
+    // if is default port
     let originUrl = config.APP_PORT == 80 ?
         `http://${config.APP_DOMAIN}` :
         `http://${config.APP_DOMAIN}:${config.APP_PORT}`;
