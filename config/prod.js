d1f41f476d0ae96cbcdbb4292c9fd176e2ad050b
Xiaozhong <xiaozhong.liu@outlook.com>
2017-03-07 17:44:55 +0800

    fix prod api domain

--- config/prod.js
+++ config/prod.js
@@ -4,11 +4,11 @@ module.exports = {
 
     // app
     APP_DOMAIN: 'www.flashbeep.cn',
-    APP_PORT: 80,
+    APP_PORT: 3000,
 
-    // api port
+    // api
     API_DOMAIN: 'api.weibo.flashbeep.cn',
-    API_PORT: 80,
+    API_PORT: 3100,
 
     //redis
     REDIS_HOST: '127.0.0.1',
--- midware/oauth.js
+++ midware/oauth.js
@@ -9,12 +9,12 @@ weibo.init('weibo', config.APP_KEY, config.APP_SECRET);
 
 module.exports = weibo.oauth({
     blogtypeField: 'type',
-    homeUrl: `http://${config.API_DOMAIN}:${config.API_PORT}`,
-    afterLogin: function (req, res, callback) {
+    homeUrl: `http://${config.API_DOMAIN}${config.DEBUG ? `:${config.API_PORT}` : ''}`,
+    afterLogin: function(req, res, callback) {
         console.log(req.session.oauthUser.screen_name, 'login success');
         process.nextTick(callback);
     },
-    beforeLogout: function (req, res, callback) {
+    beforeLogout: function(req, res, callback) {
         console.log(req.session.oauthUser.screen_name, 'loging out');
         process.nextTick(callback);
     }
